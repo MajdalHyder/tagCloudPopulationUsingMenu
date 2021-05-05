be8890501afe7805f0416f7006ec120df1d30519
Majd AL HYDER <48190132+MajdalHyder@users.noreply.github.com>
2021-05-05 10:34:11 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function addToCloud(menu) {
+    let tagCloud = document.getElementById("language-cloud")
+    let tagCloudChildCount = tagCloud.childNodes.length;
+    console.log(tagCloudChildCount);
+    for (let i = 0; i < tagCloudChildCount; i++) {
+        if (tagCloud.childNodes[i].value == menu.value) {
+            alert("already exists");
+            return;
+        }
+    }
+    let newTag = document.createElement("option");
+    newTag.setAttribute("value", menu.value);
+    let tagContent = document.createTextNode(menu.options[menu.selectedIndex].text);
+    newTag.appendChild(tagContent);
+    tagCloud.appendChild(newTag);
+}
+
+var tagCloud = document.getElementById("language-cloud");
+tagCloud.addEventListener('click', function(e) {
+    tagCloud.removeChild(e.target);
+})
+
+/*
+	Test HTML code:
+	<select id="language-menu" onChange="addToCloud(this)">
+    <option value="ar">Arabic</option>
+    <option value="en">English</option>
+    <option value="fr">French</option>
+    <option value="de">German</option>
+	</select>
+
+<div id="language-cloud">
+
+</div>
+*/
