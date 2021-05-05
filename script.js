c930985a0966f3c32a19371eef9ca734de3f8a6c
Majd AL HYDER <48190132+MajdalHyder@users.noreply.github.com>
2021-05-05 10:34:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ tagCloud.addEventListener('click', function(e) {
 /*
 	Test HTML code:
 	<select id="language-menu" onChange="addToCloud(this)">
-    <option value="ar">Arabic</option>
-    <option value="en">English</option>
-    <option value="fr">French</option>
-    <option value="de">German</option>
+    		<option value="ar">Arabic</option>
+   		<option value="en">English</option>
+    		<option value="fr">French</option>
+    		<option value="de">German</option>
 	</select>
 
-<div id="language-cloud">
+	<div id="language-cloud">
 
-</div>
+	</div>
 */
